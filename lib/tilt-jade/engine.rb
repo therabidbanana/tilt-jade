f2e55bf9a54ffef5fcecb7457fda63e20ccc8be5
David Haslem <therabidbanana@gmail.com>
2011-09-22 22:34:57 -0400

    Better non rails support

--- lib/tilt-jade/engine.rb
+++ lib/tilt-jade/engine.rb
@@ -1,18 +1,18 @@
-module Rails; class Engine; end; end
+if defined?(Rails)
+  module TiltJade
+    class Engine < Rails::Engine
+      initializer "tilt_jade.configure_rails_initialization" do |app|
+        next unless app.config.assets.enabled
 
-module TiltJade
-  class Engine < Rails::Engine
-    initializer "tilt_jade.configure_rails_initialization" do |app|
-      next unless app.config.assets.enabled
-
-      require 'sprockets'
-      require 'sprockets/engines'
-      require "tilt-jade/template"
-      if app.assets
-        app.assets.register_engine '.jade', ::TiltJade::Template
-      else
-        # Rails 3.1.0rc
-        Sprockets.register_engine '.jade', ::TiltJade::Template
+        require 'sprockets'
+        require 'sprockets/engines'
+        require "tilt-jade/template"
+        if app.assets
+          app.assets.register_engine '.jade', ::TiltJade::Template
+        else
+          # Rails 3.1.0rc
+          Sprockets.register_engine '.jade', ::TiltJade::Template
+        end
       end
     end
   end
